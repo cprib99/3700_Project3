2ed5294f5f9db4a62ffdf3c559f030784205fe03
Jordan Brown <jsfkmrocks@gmail.com>
2020-04-17 15:48:22 -0400

    Added pseudo for alg 1

--- main.cc
+++ main.cc
@@ -14,9 +14,24 @@ int main()
 
 
 //Algorithm 1 - 
+int SampleNoReplacement(list items, int n)
+{
+    //Variables:
+    int i, e;
+
+    i = Rand mod n;         //select a random position in the list
+    e = items[i];           //Remember the selected item
+    n = n-1;                //Decrement n
+    items[i] = items[n];    //Move last item into selected position
+
+    return e;
+}
 
 //Algorthm 2 - 
 
+
 //Algorithm 3 -
 
+
 //Algorithm 4 - 
+

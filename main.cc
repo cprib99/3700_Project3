a4c672852b66470d29df6139babd5708e8d29f12
Jordan Brown <jsfkmrocks@gmail.com>
2020-04-17 15:59:31 -0400

    Pseudocode for alg 2

--- main.cc
+++ main.cc
@@ -3,6 +3,9 @@
 //Project 3 - Hex Pathfinder
 
 //Prototypes
+int SampleNoReplacement(list items, int n);
+void DisjointSetUnion(elements, rank, int a, int b);
+
 
 //Globals
 
@@ -13,25 +16,59 @@ int main()
 }
 
 
-//Algorithm 1 - 
+//Algorithm 1 - Sampling without replacement
 int SampleNoReplacement(list items, int n)
 {
     //Variables:
     int i, e;
 
-    i = Rand mod n;         //select a random position in the list
-    e = items[i];           //Remember the selected item
-    n = n-1;                //Decrement n
-    items[i] = items[n];    //Move last item into selected position
+    //Code:
+    i = Rand mod n;                     //select a random position in the list
+    e = items[i];                       //Remember the selected item
+    n = n-1;                            //Decrement n
+    items[i] = items[n];                //Move last item into selected position
 
     return e;
 }
 
-//Algorthm 2 - 
+//Algorthm 2 - Disjoint set union
+void DisjointSetUnion(elements, rank, int a, int b)
+{
+    //Variables:
+
+    //Code:
+    a = DisjointSetFind(a);
+    b = DisjointSetFind(b);
+    
+    if(a != b)                          //Only union if a and b are in different sets
+    {
+        if(rank[a] < rank[b])           //Set with lower rank merged into set with larger rank
+        {
+            elements[a] = b;
+        }
+        else
+        {
+            if(rank[a] == rank[b])      //In case of tie, increment one set's rank
+            {
+                rank[a] = rank[a] + 1;
+            }
+
+            elements[b] = a;
+        }
+    }
+    
+}
 
 
-//Algorithm 3 -
+//Algorithm 3 - Disjoint set find
+int DisjointSetFind(elements, rank, a)
+{
+    //Variables:
 
+    //Code:
+
+
+}
 
 //Algorithm 4 - 
 

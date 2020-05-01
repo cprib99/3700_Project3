2f35575700278ef6d4628a5bfa3e54889b06915d
Jordan Brown <jsfkmrocks@gmail.com>
2020-05-01 18:29:19 -0400

    Added Sampler.h .....sorta

---
+++ Sampler.h
@@ -0,0 +1,21 @@
+#include <cstdint>
+#include <iostream>
+#include "fraction.h"
+
+#ifndef _SAMPLER_H
+#define _SAMPLER_H
+
+class Sampler {
+public:
+	//Constructor & Destructor num/den
+	Sampler(int n);
+	~Sampler(void) {}
+
+private:
+	n,
+	i,
+	array[i];
+};
+
+
+#endif
